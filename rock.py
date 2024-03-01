dcbf32d4c0b5412f230f6da7383fd20094258b81
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-03-01 14:25:22 +0100

    Add definitions

--- rock.py
+++ rock.py
@@ -7,7 +7,37 @@ rps.titel("Rock-Paper-Scissor | @Coerres")
 user_score = 0
 comp_score = 0
 user_choice = ""
-cpm_choice = ""
+comp_choice = ""
+
+def choice_to_number(choice):
+    rps = {'rock':0, 'paper':1, 'scissor':2}
+def number_to_choice(number):
+    rps = {0: 'rock', 1: 'paper', 2: 'scissor'}
+
+def random_computer_choice():
+    return random.choice(['rock', 'paper', 'scissor'])
+
+
+def rock():
+    global user_choice
+    global comp_choice
+    user_choice = "rock"
+    comp_choice = random_computer_choice()
+    result(user_choice, comp_choice)
+
+def paper():
+    global user_choice
+    global comp_choice
+    user_choice = "rock"
+    comp_choice = random_computer_choice()
+    result(user_choice, comp_choice)
+
+def scissor():
+    global user_choice
+    global comp_choice
+    user_choice = "rock"
+    comp_choice = random_computer_choice()
+    result(user_choice, comp_choice)
 
 #Create label and buttons
 button_rock = Button(text="      ROCK      ", bg = "#808487",font = ("arial",15,"italic bold"), relief = RIDGE,
